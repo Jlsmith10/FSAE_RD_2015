ff19503d78c7cf93d2526fe451c7f9da8785b26b
Niral Pathak <niralpathak@gmail.com>
2016-02-21 11:09:16 -0800

    worked on BerryIMY code

--- BerryIMU Code/FSAE IMU Data Acq/acq_raw_data.c
+++ BerryIMU Code/FSAE IMU Data Acq/acq_raw_data.c
@@ -24,27 +24,32 @@ void INThandler(int sig)
     exit(0);
 }
 
-int mymillis()
+int myMicro();
+
+int myMicro()
 {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return (tv.tv_sec) * 1000 + (tv.tv_usec)/1000;
+
+    //tv.sec is the time from 1970 and tv_usec is microseconds which according
+    //to the api are not to accurate
+    return (tv.tv_sec)*1000000 + (tv.tv_usec);
 }
 
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+/*int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
 {
     long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
     result->tv_sec = diff / 1000000;
     result->tv_usec = diff % 1000000;
     return (diff<0);
-}
+}*/
 
 int main(int argc, char *argv[])
 {
     int  accRaw[3];
     int  gyrRaw[3];
 
-    int startInt  = mymillis();
+    int startInt  = myMicro();
     struct  timeval tvBegin, tvEnd,tvDiff;
 
     FILE *theFile;
@@ -55,33 +60,32 @@ int main(int argc, char *argv[])
 
     gettimeofday(&tvBegin, NULL);
 
-    //get real time clock info -> fopen a file with the date as its name with "a"
-    theFile = fopen("testFile", "a");
+    //get REAL TIME CLOCK info -> fopen a file with the date as its name with "a"
+    theFile = fopen("dataFiles/testFile", "w");
     
-    //startInt = mymillis();
+    //startInt = myMicro();
 
     while(1)//run until the button is pressed
     {
-        startInt = mymillis();
-
+        startInt = myMicro();
 
         //read ACC and GYR data
         readACC(accRaw);
         readGYR(gyrRaw);
 
-        //binary data printed
-        fwrite(accRaw, sizeof(int), 3, theFile);
-        fwrite(gyrRaw, sizeof(int), 3, theFile)
-        //write the time????
-
+        //writing accRaw[0], accRaw[1], accRaw[2]
+        //then gyrRaw[0], gyrRaw[1], gyrRaw[2]
+        //then the time difference
+        fprintf(theFile, "%d %d %d %d %d %d %d\n", accRaw[0], accRaw[1], accRaw[2], gyrRaw[0], gyrRaw[1], gyrRaw[2], (myMicro() - startInt));
 
         //Each loop should be at least 20ms.
-        while(mymillis() - startInt < (DT*1000))
+        while(myMicro() - startInt < (DT*100000))
         {
+            printf("%s", "sleeping");
+            fflush(stdout);
             usleep(100); //arg in microsec
         }
 
-        printf("Loop Time %d\t", mymillis()- startInt);
     }
 
     // fclose file that was opened
